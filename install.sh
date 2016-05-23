54a9295e64a1347e0b56e664364ed1d49eef9f64
James Chow <zhouxy.vortex@gmail.com>
2016-05-23 13:37:57 +0800

    update shell script

--- install.sh
+++ install.sh
@@ -13,12 +13,12 @@ if which brew >/dev/null; then
 fi
 
 cp ~/.vimrc ~/.vimrc.bak
-mv -f ~/vim ~/vim_old
-cd ~/ && git clone https://github.com/jamechou/vim
+mv -f ~/jc-vim ~/jc-vim_old
+cd ~/ && git clone https://github.com/jamechou/jc-vim
 mv -f ~/.vim ~/.vim_old
-mv -f ~/.vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
-mv -f ~/.vim/.vimrc ~/
+mv -f ~/jc-vim/vimrc ~/.vimrc
+mkdir ~/.vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 echo "正在安装相关插件，可能会花费几分钟的时间请稍候~" >> log
 echo "安装完成后，会自动退出!" >> log
--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,7 @@ rm -fr ~/vim
 rm -fr ~/.vim
 rm -fr ~/vim_old
 rm -f ~/.vimrc
+rm -fr ~/jc-vim
 mv ~/.vimrc.bak ~/.vimrc
 
 echo "卸载完成!"
